7ddfd94c25917451a61754a9c5542356592d285a
Terry Hearst <thearst3rd@gmail.com>
2024-08-16 23:23:57 -0400

    Add basic main menu and credits menu

---
+++ src/menu/main_menu.gd
@@ -0,0 +1,24 @@
+extends CenterContainer
+
+
+signal show_credits()
+
+@onready var quit_button: Button = %QuitButton
+
+
+func _ready() -> void:
+	if OS.has_feature("web"):
+		quit_button.hide()
+
+
+func _on_play_button_pressed() -> void:
+	#get_tree().change_scene_to_file("res://path/to/game/scene.tscn")
+	pass
+
+
+func _on_credits_button_pressed() -> void:
+	show_credits.emit()
+
+
+func _on_quit_button_pressed() -> void:
+	get_tree().quit()
